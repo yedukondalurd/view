25618e312002efc115ff0af89b1d89fa23cb524a
yedukondalu <yedukondalu.vanipenta@visenti.com>
2018-08-28 21:42:12 +0800

    init

--- app/containers/LanguageProvider/index.js
+++ app/containers/LanguageProvider/index.js
@@ -39,13 +39,4 @@ const mapStateToProps = createSelector(makeSelectLocale(), locale => ({
   locale,
 }));
 
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(LanguageProvider);
+export default connect(mapStateToProps)(LanguageProvider);
